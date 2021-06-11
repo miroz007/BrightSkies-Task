6ac36819819a344186304b5f72f7f770b91ca3c3
miroz007 <crazy_thunder7@yahoo.com>
2021-06-11 17:20:23 +0200

    Initial Commit

---
+++ BrightSkies-Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrightSkies-Task
+//
+//  Created by Amir on 6/11/21.
+//  Copyright © 2021 Amir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
